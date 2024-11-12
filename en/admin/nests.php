9191b82844b14047e09b5b01b04b64661bbb359a
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2024-11-12 11:56:55 +0100

    Update nests.php

--- en/admin/nests.php
+++ en/admin/nests.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * Pterodactyl - Panel
+ * Pterodactyl - Panneau
  * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
  *
- * This software is licensed under the terms of the MIT license.
+ * Ce logiciel est sous licence MIT.
  * https://opensource.org/licenses/MIT
  */
 
 return [
     'notices' => [
-        'created' => 'A new nest, :name, has been successfully created.',
-        'deleted' => 'Successfully deleted the requested nest from the Panel.',
-        'updated' => 'Successfully updated the nest configuration options.',
+        'created' => 'Un nouveau nid, :name, a été créé avec succès.',
+        'deleted' => 'Le nid demandé a été supprimé avec succès du panneau.',
+        'updated' => 'Les options de configuration du nid ont été mises à jour avec succès.',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Successfully imported this Egg and its associated variables.',
-            'updated_via_import' => 'This Egg has been updated using the file provided.',
-            'deleted' => 'Successfully deleted the requested egg from the Panel.',
-            'updated' => 'Egg configuration has been updated successfully.',
-            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+            'imported' => 'Cet œuf et ses variables associées ont été importés avec succès.',
+            'updated_via_import' => 'Cet œuf a été mis à jour en utilisant le fichier fourni.',
+            'deleted' => 'L\'œuf demandé a été supprimé avec succès du panneau.',
+            'updated' => 'La configuration de l\'œuf a été mise à jour avec succès.',
+            'script_updated' => 'Le script d\'installation de l\'œuf a été mis à jour et sera exécuté lors de l\'installation des serveurs.',
+            'egg_created' => 'Un nouvel œuf a été pondu avec succès. Vous devrez redémarrer les daemons en cours d\'exécution pour appliquer ce nouvel œuf.',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'La variable ":variable" a été supprimée et ne sera plus disponible pour les serveurs une fois reconstruits.',
+            'variable_updated' => 'La variable ":variable" a été mise à jour. Vous devrez reconstruire les serveurs utilisant cette variable pour appliquer les changements.',
+            'variable_created' => 'Une nouvelle variable a été créée avec succès et assignée à cet œuf.',
         ],
     ],
 ];
