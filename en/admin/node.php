684d80ce81d6a69e08c2d1e05730cf6f68eda173
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2024-11-12 11:57:45 +0100

    Update node.php

--- en/admin/node.php
+++ en/admin/node.php
@@ -1,23 +1,24 @@
 <?php
 /**
- * Pterodactyl - Panel
+ * Pterodactyl - Panneau
  * Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com>.
  *
- * This software is licensed under the terms of the MIT license.
+ * Ce logiciel est sous licence MIT.
  * https://opensource.org/licenses/MIT
  */
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'Le nom de domaine complet (FQDN) ou l\'adresse IP fournie ne résout pas vers une adresse IP valide.',
+        'fqdn_required_for_ssl' => 'Un nom de domaine complet qui résout vers une adresse IP publique est requis pour utiliser SSL pour ce nœud.',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
-        'node_deleted' => 'Node has been successfully removed from the panel.',
-        'location_required' => 'You must have at least one location configured before you can add a node to this panel.',
-        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
-        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
-        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
+        'allocations_added' => 'Les allocations ont été ajoutées avec succès à ce nœud.',
+        'node_deleted' => 'Le nœud a été supprimé avec succès du panneau.',
+        'location_required' => 'Vous devez avoir au moins un emplacement configuré avant de pouvoir ajouter un nœud à ce panneau.',
+        'node_created' => 'Nouveau nœud créé avec succès. Vous pouvez configurer automatiquement le daemon sur cette machine en visitant l\'onglet "Configuration". <strong>Avant de pouvoir ajouter des serveurs, vous devez d\'abord allouer au moins une adresse IP et un port.</strong>',
+        'node_updated' => 'Les informations du nœud ont été mises à jour. Si des paramètres du daemon ont été modifiés, vous devrez le redémarrer pour que les changements prennent effet.',
+        'unallocated_deleted' => 'Tous les ports non alloués ont été supprimés pour <code>:ip</code>.',
     ],
 ];
+
